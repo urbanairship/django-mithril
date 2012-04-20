ce262b7375335675bfb0d3e57411bf15fa79bc50
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-04-20 13:11:39 -0700

    add fmt_ip docstring

--- mithril/tests/utils.py
+++ mithril/tests/utils.py
@@ -5,6 +5,14 @@ def fake_settings(**kwargs):
 
 
 def fmt_ip(ip_int):
+    """
+        ``ip_int`` is a 32-bit integer representing an IP with octets A.B.C.D
+        arranged like so::
+
+            A << 24 | B << 16 | C << 8 | D
+
+        returns the number formatted as an IP address string.
+    """
     return '%d.%d.%d.%d' % (
         (ip_int & (0xFF << 24)) >> 24,
         (ip_int & (0xFF << 16)) >> 16,
